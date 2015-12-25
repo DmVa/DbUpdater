69763a8c57606755bcb858829cb6505d3b215265
DmVa <dmytro.vasylenko@preciseres.com>
2015-12-25 16:11:06 +0200

    add multiple source

---
+++ DBUpdater/Configuration/DbUpdaterConfigurationElementCollection.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBUpdater.Configuration
+{
+    public class DbUpdaterConfigurationElementCollection : ConfigurationElementCollection
+    {
+        protected override ConfigurationElement CreateNewElement()
+        {
+            return new DbUpdaterConfigurationSection();
+        }
+
+        protected override object GetElementKey(ConfigurationElement element)
+        {
+            return ((DbUpdaterConfigurationSection)element).GetHashCode();
+        }
+        public DbUpdaterConfigurationSection this[int index]
+        {
+            get { return (DbUpdaterConfigurationSection)BaseGet(index); }
+        }
+
+    }
+}
---
+++ DBUpdater/Configuration/DbUpdaterMultipleSourceConfigurationSection.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBUpdater.Configuration
+{
+    public class DbUpdaterMultipleSourceConfigurationSection : ConfigurationSection
+    {
+        public override bool IsReadOnly()
+        {
+            return false;
+        }
+
+        [ConfigurationProperty("ConnectionString", DefaultValue = "", IsRequired = false)]
+        public String ConnectionString
+        {
+            get { return (String)this["ConnectionString"]; }
+            set { this["ConnectionString"] = value; }
+        }
+
+        [ConfigurationProperty("Sources", IsRequired = false, IsDefaultCollection=false)]
+        [ConfigurationCollection(typeof(DbUpdaterConfigurationElementCollection), AddItemName = "DbUpdater")]
+        public DbUpdaterConfigurationElementCollection DbUpdaterConfigurations
+        {
+            get { return (DbUpdaterConfigurationElementCollection)this["Sources"]; }
+        }
+    }
+}
